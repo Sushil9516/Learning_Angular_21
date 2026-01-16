9f7a1d7e041f7db1395ff058e4a2aeed2c875e77
sushil <sonisushil9516@gmail.com>
2026-01-16 23:14:53 +0530

    16-1-2026

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Login } from './login/login';
+import { Signup } from "./signup/signup";
+import { Counter } from './counter/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Counter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/counter/counter.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html',
+  styleUrl: './counter.css',
+})
+export class Counter {
+  n = 0;
+  // decrement() {
+  //   this.n = this.n - 1;
+  // }
+  // increment() {
+  //   this.n = this.n + 1;
+  // }
+  // reset() {
+  //   this.n = 0;
+  // }
+  handleCounter(val: string) {
+    if (val == "minus") {
+      if (this.n > 0) this.n = this.n - 1;
+    }
+    else if (val == "plus") this.n = this.n + 1;
+    else this.n = 0;
+
+  }
+}
